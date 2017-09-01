60a8ab257fca913ef1e6ba42712736edf90a9838
yestoryfornever <guosj90@163.com>
2017-09-01 16:26:40 +0800

    更新待办

--- src/index.jsx
+++ src/index.jsx
@@ -78,9 +78,12 @@ render(
 
 /**
  * 路由缓存
- * css module
+ * 富文本编辑器
+ * markdown
  * redux-gen - 生成器
  * react-redux-form - 表单提交
+ * 
+ * #css module
  * #react-router-redux - 路由
  * #extract-text-webpack-plugin
  * #redux-thunk - 实现异步action （ajax）
