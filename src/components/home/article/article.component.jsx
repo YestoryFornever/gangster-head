d5f1a96376a05aeae1d81e8f094cfb04cd195cd2
YestoryFornever <guosj90@163.com>
2017-09-03 21:20:31 +0800

    navigate

--- src/components/home/article/article.component.jsx
+++ src/components/home/article/article.component.jsx
@@ -1,6 +1,8 @@
 import './article.less'
 import React, { Component, PropTypes } from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
+
+import { history } from 'alias_utils/js/history'
 
 import ArticleList from './article-list/article-list.container'
 import ArticleHit from './article-hit/article-hit.container'
@@ -8,7 +10,16 @@ import ArticleRun from './article-run/article-run.container'
 const Home = ({match}) => {
 	return (
 		<div>
-            <Route exact path={match.url} component={ArticleList} />
+			<button onClick={e=>{
+					history.push('./article-hit')
+				}
+			}>aaa</button>
+			<Route exact path={match.url} render={()=>(
+				<Redirect to={`${match.url}/article-list`} />
+			)} />
+            	<Route path={`${match.url}/article-list`} component={ArticleList} />
+			<Route path={`${match.url}/article-hit`} component={ArticleHit} />
+			<Route path={`${match.url}/article-run`} component={ArticleRun} />
 		</div>
 	)
 };
