7c02a29f39730ab05a3c03be9fc9b50f95349aa4
yestoryfornever <guosj90@163.com>
2017-09-15 10:16:35 +0800

    animation demo

--- src/components/error/error.component.jsx
+++ src/components/error/error.component.jsx
@@ -2,15 +2,18 @@ import React, { Component, PropTypes } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './error.less'
 
+import Animation from 'alias_utils/demo/animation'
+
 const Error = ({}) => {
 	return (
 		<div className="f-container">
-			<div id="f1" className="fff">f1</div>
+			<Animation />
+			{/* <div id="f1" className="fff">f1</div>
 			<div id="f2" className="fff">f2</div>
 			<div id="f3" className="fff">f3</div>
 			<div id="f4" className="fff">f4</div>
 			<div id="f5" className="fff">f5</div>
-			<h1 className="test-text">测试用文本，words for test</h1>
+			<h1 className="test-text">测试用文本，words for test</h1> */}
 		</div>
 	)
 };
