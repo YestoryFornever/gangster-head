7d8bd55822ae58db3c10ac2f7e102f69354d4571
YestoryFornever <guosj90@163.com>
2017-09-17 18:45:43 +0800

    fade

--- src/components/home/article/article.component.jsx
+++ src/components/home/article/article.component.jsx
@@ -1,22 +1,52 @@
 import './article.less'
 import React, { Component, PropTypes } from 'react'
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import ArticleList from './article-list/article-list.container'
 import ArticleHit from './article-hit/article-hit.container'
 import ArticleRun from './article-run/article-run.container'
 const Home = ({ match, saveModalStatus, openSaveModal, closeSaveModal}) => {
 	return (
-		<div className="article-component">
-			<Route exact path={match.url} render={() => (
-				<Redirect to={`${match.url}/article-list`} />
-			)} />
-			<Route path={`${match.url}/article-list`} component={ArticleList} />
-			{/* 顺序很重要，自上而下匹配 */}
-			<Route path={`${match.url}/article-hit/:id?`} component={ArticleHit} />
-			{/* <Route path={`${match.url}/article-hit`} component={ArticleHit} /> */}
-			<Route path={`${match.url}/article-run/:id`} component={ArticleRun} />
-		</div>
+		<Route render={({ location }) => {
+			return (
+				<div className="article-component">
+					<Route exact path={match.url} render={() => (
+						<Redirect to={`${match.url}/article-list`} />
+					)} />
+					<ReactCSSTransitionGroup
+						transitionName="cartoon-fade" 
+						transitionEnterTimeout={2000}
+						transitionLeaveTimeout={0}
+						>
+						<Route location={location}
+							key={location.key} 
+							path={`${match.url}/article-list`} 
+							component={ArticleList} />
+					</ReactCSSTransitionGroup>
+					<ReactCSSTransitionGroup
+						transitionName="cartoon-fade" 
+						transitionEnterTimeout={2000}
+						transitionLeaveTimeout={0}
+						>
+						{/* 顺序很重要，自上而下匹配 */}
+						<Route location={location}
+							key={location.key} 
+							path={`${match.url}/article-hit/:id?`} 
+							component={ArticleHit} />
+					</ReactCSSTransitionGroup>
+					<ReactCSSTransitionGroup
+						transitionName="cartoon-fade" 
+						transitionEnterTimeout={2000}
+						transitionLeaveTimeout={0}
+						>
+						<Route location={location}
+							key={location.key} 
+							path={`${match.url}/article-run/:id`} 
+							component={ArticleRun} />
+					</ReactCSSTransitionGroup>
+				</div>
+			)
+		}} />
 	)
 };
 export default Home;
