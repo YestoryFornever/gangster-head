42b3d5216e75e2347e449cde566eeca22ffca14c
YestoryFornever <guosj90@163.com>
2017-09-30 15:53:49 +0800

    404

--- src/components/error/error.component.jsx
+++ src/components/error/error.component.jsx
@@ -7,13 +7,14 @@ import './error.less'
 const Error = ({}) => {
 	return (
 		<div className="f-container">
+			404
 			{/* <Animation /> */}
-			<div id="f1" className="fff">f1</div>
+			{/* <div id="f1" className="fff">f1</div>
 			<div id="f2" className="fff">f2</div>
 			<div id="f3" className="fff">f3</div>
 			<div id="f4" className="fff">f4</div>
 			<div id="f5" className="fff">f5</div>
-			<h1 className="test-text">测试用文本，words for test</h1>
+			<h1 className="test-text">测试用文本，words for test</h1> */}
 		</div>
 	)
 };
