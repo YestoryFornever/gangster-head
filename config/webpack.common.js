37e7854e20b95b1908496eac216e93dc75edbf8a
YestoryFornever <guosj90@163.com>
2018-01-15 16:37:41 +0800

    update ExtractTextPlugin

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -71,55 +71,59 @@ module.exports = (opt) => {
 				},
 				{
 					test: /\.css$/,
-					use:[
-						'style-loader',
-						{
-							loader:'css-loader',
-							options: {
-								modules:false,
+					use: ExtractTextPlugin.extract({
+						fallback: "style-loader",
+						use:[
+							{
+								loader:'css-loader',
+								options: {
+									modules:false,
+								}
+							},
+							{
+								loader: 'postcss-loader',
+								options: {
+									browsers: ["last 2 version"]
+								}
 							}
-						},
-						{
-							loader: 'postcss-loader',
-							options: {
-								browsers: ["last 2 version"]
-							}
-						}
-					],
+						],
+					})
 					//include: helpers.root('src')//白名单
 				},
 				{
 					test: /\.less$/,
-					use: [
-						'style-loader',
-						{
-							loader:'css-loader',
-							options: {
-								modules:false,
-								/* alias: {// 未成功
-									alias_materials:helpers.root('src/materials')
-								} */
+					use: ExtractTextPlugin.extract({
+						fallback: "style-loader",
+						use: [
+							{
+								loader:'css-loader',
+								options: {
+									modules:false,
+									/* alias: {// 未成功
+										alias_materials:helpers.root('src/materials')
+									} */
+								}
+							},
+							{
+								loader: 'postcss-loader',
+								options: {
+									sourceMap: true,
+									browsers: ["last 2 version"]
+								}
+							},
+							{
+								loader:'less-loader',
+								options:{
+									sourceMap:true,
+									modifyVars:theme,
+									paths:[
+										helpers.root('node_modules'),
+										helpers.root('src/utils/styles'),
+									]
+								}
 							}
-						},
-						{
-							loader: 'postcss-loader',
-							options: {
-								sourceMap: true,
-								browsers: ["last 2 version"]
-							}
-						},
-						{
-							loader:'less-loader',
-							options:{
-								sourceMap:true,
-								modifyVars:theme,
-								paths:[
-									helpers.root('node_modules'),
-									helpers.root('src/utils/styles'),
-								]
-							}
-						}
-					],
+						],
+					})
 				},
 				{
 					test: /\.(ico|png|jpe?g|gif|svg|woff|woff2|ttf|wav|mp3)$/,
@@ -155,7 +159,8 @@ module.exports = (opt) => {
 			}),
 			new CopyWebpackPlugin([
 				{ from: 'src/materials', to: 'materials' },
-			])
+			]),
+			new ExtractTextPlugin('[name].css')
 		],
 	}
 }; 
